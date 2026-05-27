d33b52754cc3af6b9b2dcaac86476b961d48cdac
Danielsb100 <daniel_silva_baptista@outlook.com>
2026-05-27 10:25:47 -0300

    chore: remove obsolete temporary scripts and utility files from the repository

--- scratch/check_key.js
+++
@@ -1,6 +0,0 @@
-const prisma = require('../config/db.js');
-async function main() {
-    const doc45 = await prisma.document.findUnique({where: {id: 45}});
-    console.log('Doc 45:', doc45 ? {id: doc45.id, storageKey: doc45.storageKey} : 'not found');
-}
-main().finally(() => prisma.$disconnect());
