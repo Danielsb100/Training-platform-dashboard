ee909baf81d59421e22a94ddfba688da7a8dd0d4
Danielsb100 <daniel_silva_baptista@outlook.com>
2026-07-09 21:31:47 -0300

    feat: enable trust proxy setting in Express app configuration

--- server.js
+++ server.js
@@ -147,6 +147,7 @@ const handleUploadDocumentToDiskError = (fieldName) => (req, res, next) => {
 };
 
 const app = express();
+app.set('trust proxy', 1);
 const server = http.createServer(app);
 
 const io = new Server(server, {
